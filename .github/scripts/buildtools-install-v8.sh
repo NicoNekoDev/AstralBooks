f1d0894b70bf6034ef9127cc59ab3c0d71142860
NicoNekoDev <nicuch100@gmail.com>
2021-08-31 18:20:42 +0300

    Maven GitHub workflow fixing 3.2

--- .github/scripts/buildtools-install-v8.sh
+++ .github/scripts/buildtools-install-v8.sh
@@ -6,19 +6,19 @@ else
 	wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 fi
 echo "[INFO} Installing spigot to maven..."
-java -jar BuildTools.jar --rev 1.8
-java -jar BuildTools.jar --rev 1.8.3
-java -jar BuildTools.jar --rev 1.8.8
-java -jar BuildTools.jar --rev 1.9
-java -jar BuildTools.jar --rev 1.9.2
-java -jar BuildTools.jar --rev 1.9.4
-java -jar BuildTools.jar --rev 1.10.2
-java -jar BuildTools.jar --rev 1.11.2
-java -jar BuildTools.jar --rev 1.12.2
-java -jar BuildTools.jar --rev 1.13
-java -jar BuildTools.jar --rev 1.13.2
-java -jar BuildTools.jar --rev 1.14.4
-java -jar BuildTools.jar --rev 1.15.2
-java -jar BuildTools.jar --rev 1.16.1
-java -jar BuildTools.jar --rev 1.16.3
-java -jar BuildTools.jar --rev 1.16.5
+java -jar BuildTools.jar --rev 1.8 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.8.3 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.8.8 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.9 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.9.2 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.9.4 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.10.2 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.11.2 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.12.2 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.13 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.13.2 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.14.4 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.15.2 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.16.1 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.16.3 --compile craftbukkit
+java -jar BuildTools.jar --rev 1.16.5 --compile craftbukkit
