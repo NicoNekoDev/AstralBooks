d2a3b068a23266ef4811ac49a7338ac01aaec9ef
NicoNekoDev <nicuch100@gmail.com>
2023-06-10 22:17:26 +0300

    Update/Add copyright.

--- astralbooks-core/src/main/java/ro/niconeko/astralbooks/storage/types/EmbedStorage.java
+++ astralbooks-core/src/main/java/ro/niconeko/astralbooks/storage/types/EmbedStorage.java
@@ -1,3 +1,20 @@
+/*
+ *     CitizensBooks
+ *     Copyright (c) 2023 @ Drăghiciu 'NicoNekoDev' Nicolae
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
 package ro.niconeko.astralbooks.storage.types;
 
 import io.github.NicoNekoDev.SimpleTuples.Pair;
@@ -16,13 +33,13 @@ import java.util.logging.Level;
 public abstract class EmbedStorage extends Storage {
     protected String fileName;
     protected boolean encryptionEnabled;
-
     protected int autoSaveInterval;
 
     protected EmbedStorage(AstralBooksPlugin plugin, StorageType storageType) {
         super(plugin, storageType);
     }
 
+    @Override
     protected void loadSettings(StorageSettings settings) {
         StorageEmbedSettings embedSettings = settings.getEmbedSettings();
         this.fileName = embedSettings.getFileName();
@@ -31,6 +48,7 @@ public abstract class EmbedStorage extends Storage {
         super.purgeSecurityBooksOlderThan = settings.getSecurityBookPurgeOlderThan();
     }
 
+    @Override
     protected boolean preloadCache() {
         try (PreparedStatement statement = super.connection.prepareStatement(
                 "SELECT filter_name FROM 'filters';"
